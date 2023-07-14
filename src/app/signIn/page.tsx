7bee99cc936b51337752660b7f2873a48e78ee14
suhyeong.ahn <tngud102@naver.com>
2023-07-15 00:34:21 +0900

    Applied dynamic functions with components in page

--- src/app/signIn/page.tsx
+++ src/app/signIn/page.tsx
@@ -1,13 +1,13 @@
 'use client'
 
 import dynamic from 'next/dynamic';
-const Background = dynamic(() => import('@signIn/background'), {
-  ssr: false
-});
 
+const Background = dynamic(() => import('@signIn/background'), { ssr: false });
+const SocialSignInForm = dynamic(() => import('@signIn/socialSignInForm'));
+const Header = dynamic(() => import('@signIn/header'));
 // import Background from '@signIn/background';
-import SocialSignInForm from '@signIn/socialSignInForm';
-import Header from '@signIn/header';
+// import SocialSignInForm from '@signIn/socialSignInForm';
+// import Header from '@signIn/header';
 import Footer from 'src/app/footer';
 
 const SignIn: React.FC = () => {
