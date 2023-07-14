2244dbded5ec53ab2c9c8c67a46e6abaeaa2750e
suhyeong.ahn <tngud102@naver.com>
2023-07-15 01:37:02 +0900

    Applied font opt. to footer

--- src/app/footer.tsx
+++ src/app/footer.tsx
@@ -1,8 +1,14 @@
+import localFont from 'next/font/local';
+
+const Tenada = localFont({
+  src: './assets/fonts/Tenada.woff'
+});
+
 const Footer: React.FC = () => {
   
   return (
     <div className={`
-      font-['Tenada'] w-screen text-center text-gray-400 fixed bottom-0 z-10 p-3 cursor-default
+      ${ Tenada.className } w-screen text-center text-gray-400 fixed bottom-0 z-10 p-3 cursor-default
       laptop:text-[12px]
       tablet:text-[10px]
       mobile:text-[8px]
