8b3deaff9053d7ce6954a8a8cdff5dffd14c8abd
suhyeong.ahn <tngud102@naver.com>
2023-07-16 22:02:27 +0900

    Applied script strategy temporarily

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout({
         <meta name="apple-mobile-web-app-capable" content="yes"></meta>
         <Script
           src={`https://openapi.map.naver.com/openapi/v3/maps.js?ncpClientId=${process.env.NEXT_PUBLIC_NAVER_MAP_KEY}`}
-          // strategy="beforeInteractive"
+          strategy="beforeInteractive"
         ></Script>
       </head>
       <body>
