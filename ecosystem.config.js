4136a26e9047d983e2d8b09f70da98e5b70089f2
suhyeong.ahn <tngud102@naver.com>
2023-07-26 17:29:34 +0900

    Fixed ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -9,7 +9,7 @@ module.exports = {
       watch: false,
       env: {
         PORT: 3000,
-        NODE_ENV: 'development'
+        NODE_ENV: 'production'
       },
       env_production: {
         PORT: 3000,
