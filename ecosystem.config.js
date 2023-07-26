2cca00f19505d2662355ce412d2162914820f200
suhyeong.ahn <tngud102@naver.com>
2023-07-26 17:19:07 +0900

    Added pm2 ecosystem.config.js

---
+++ ecosystem.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  apps: [
+    {
+      name: 'matket-list-app',
+      script: 'npm',
+      args: 'run start',
+      instances: 2,
+      autorestart: true,
+      watch: false,
+      env: {
+        PORT: 3000,
+        NODE_ENV: 'development'
+      },
+      env_production: {
+        PORT: 3000,
+        NODE_ENV: 'production',
+      },
+      output: "./logs/console.log",
+      error: "./logs/consoleError.log",
+    },
+  ],
+};
