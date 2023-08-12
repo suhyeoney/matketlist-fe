c095d9ea1a04c0b7b8ddd3d069aeefb9be5c7d55
suhyeong.ahn <tngud102@naver.com>
2023-08-13 00:09:48 +0900

    Hotfix > Modified footer css

--- src/app/footer.tsx
+++ src/app/footer.tsx
@@ -13,25 +13,22 @@ const Footer: React.FC = () => {
 
   return (
     <>
-      <div
-        id="footer" 
-        className={`
-        ${ Tenada.className } w-screen text-center text-gray-400 fixed bottom-0 z-5 p-3 cursor-default
-        laptop:text-[12px]
-        tablet:text-[10px]
-        mobile:text-[8px]
-        smallest:text-[3px]
-      `}>
-        <div>
-          Matket List <br />
-          © 2023. suhyeong.ahn all rights reserved.
-        </div>
-      </div>
-      <div
+    <div
+      id="footer" 
       className={`
-      absolute z-5 top-0 right-2 text-gray-400 cursor-default text-[10px]
+      ${ Tenada.className } 
+      flex flex-col items-center justify-center
+      w-screen text-center text-gray-400 fixed bottom-0 z-5 p-3 cursor-default
+      laptop:text-[12px]
+      tablet:text-[10px]
+      mobile:text-[8px]
+      smallest:text-[3px]
     `}>
-      <span>{ signedInTime }</span>
+      <div>
+        Matket List <br />
+        © 2023. suhyeong.ahn all rights reserved.
+      </div>
+      <span className="self-end">{ signedInTime }</span>
     </div>
   </>
   );
