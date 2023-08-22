74bf96c034a7ba01a42634866752e5996152c339
suhyeong.ahn <tngud102@naver.com>
2023-08-23 00:21:24 +0900

    Fixed IO root margin

--- src/app/signIn/header.tsx
+++ src/app/signIn/header.tsx
@@ -48,7 +48,7 @@ const Header: React.FC = () => {
           };
         }
       },
-      { threshold: 0, root: null, rootMargin: `${ clientHeight }px 0px 0px 0px`, }
+      { threshold: 0, root: null, rootMargin: `0px 0px 0px 0px`, }
     );
     io1.observe(elementMiddleTextGroup);
   };
